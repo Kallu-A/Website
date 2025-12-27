75e255bab1804203b7c16c5e3acd2b6c521195f4
Kallu-A <lucas.aries@protonmail.com>
2025-12-27 23:43:11 +0100

    feat: add init lang

---
+++ app/page.tsx
@@ -0,0 +1,22 @@
+// app/page.tsx
+"use client";
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+
+export default function RootPage() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const preferredLanguage = window.navigator.language.startsWith("fr")
+      ? "fr"
+      : "en";
+    router.replace(`/${preferredLanguage}`);
+  }, [router]);
+
+  return (
+    <head>
+      <meta httpEquiv="refresh" content="0; url=/en" />
+    </head>
+  );
+}
