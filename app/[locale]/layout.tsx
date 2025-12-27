80a37d9e0111ad242b55973c19bb130e963437c2
Kallu-A <lucas.aries@protonmail.com>
2025-12-27 23:21:51 +0100

    fix: prod i18n

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,37 +1,28 @@
+// app/[locale]/layout.tsx
+import { ReactNode } from "react";
 import { NextIntlClientProvider } from "next-intl";
-import type { ReactNode } from "react";
+import type { LayoutProps } from "next-intl";
 
 import LanguageToggle from "./components/LanguageToggle";
 import ThemeToggle from "./components/ThemeToggle";
 
-export const generateStaticParams = async () => {
-  return [{ locale: "en" }, { locale: "fr" }];
-};
-
-export default async function LangLayout({
+export default async function LocaleLayout({
   children,
   params,
-}: {
-  children: ReactNode;
-  params: { locale: string };
-}) {
-  const { locale } = await params;
+}: LayoutProps<"/[locale]">) {
+  // react use for locale param
+  var param = await params;
 
-  if (!["en", "fr"].includes(locale)) {
-    return null;
-  }
+  const messages = (await import(`../../res/${param.locale}.json`)).default;
 
-  const messages = (await import(`../../locales/${locale}.json`)).default;
-
+  // Return wrapped children
   return (
-    <div>
-      <NextIntlClientProvider locale={locale} messages={messages}>
-        <div className="toggleContainer">
-          <LanguageToggle />
-          <ThemeToggle />
-        </div>
-        {children}
-      </NextIntlClientProvider>
-    </div>
+    <NextIntlClientProvider locale={param.locale} messages={messages}>
+      <div className="toggleContainer">
+        <LanguageToggle />
+        <ThemeToggle />
+      </div>
+      {children}
+    </NextIntlClientProvider>
   );
 }
